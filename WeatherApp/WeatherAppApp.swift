98951a8df0ead464373fb08878df7be64216e1cc
Nestor Camela <nestorcamela@gmail.com>
2025-12-23 16:10:37 -0300

    init commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nestor Camela on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
